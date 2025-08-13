e0841a4057a13595ccc95292c846d3fe110023f1
Sebastian Velasco <sesbas778@gmail.com>
2025-08-13 15:00:58 -0500

    health check configured

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { Logger, RequestMethod, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config';
 import { RpcCustomExceptionFilter } from './common';
@@ -7,13 +7,18 @@ import { RpcCustomExceptionFilter } from './common';
 async function bootstrap() {
   const logger = new Logger('ClientGatewayMain', { timestamp: true });
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '', method: RequestMethod.GET }],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  console.log('Health Check configured');
+
   app.useGlobalFilters(new RpcCustomExceptionFilter());
   await app.listen(envs.port);
   logger.log(`Gateway running on port ${envs.port}`);
